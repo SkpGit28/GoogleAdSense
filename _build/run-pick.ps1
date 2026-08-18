a9195e5d0d0ae631d100c100507d3f04086cfa9f
Sushant Kumar <skponpurpose@gmail.com>
2026-08-18 21:10:27 +0530

    chore: stop tracking _build and .py files

--- _build/run-pick.ps1
+++
@@ -1,3 +0,0 @@
-$ErrorActionPreference = "Stop"
-$pwd = (Get-Item .).FullName
-& "$pwd\_build\commons-pick.ps1" -CandidatesJson "$pwd\_build\candidates3.json" -OutDir "$pwd\images\hotels" -Manifest "$pwd\_build\manifest3.json"
